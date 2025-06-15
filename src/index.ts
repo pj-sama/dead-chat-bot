460a862f4c8bf1c25a5bedd37a2775ac294242b7
Pj Katema <panashe.katema@gmail.com>
2025-06-15 09:56:31 +0100

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {SapphireClient} from '@sapphire/framework';
 import {GatewayIntentBits} from 'discord.js';
+const express = require('express');
 import 'dotenv/config';
 
 const client = new SapphireClient({
@@ -12,3 +13,9 @@ const client = new SapphireClient({
 });
 
 void client.login(process.env.DISCORD_BOT_TOKEN);
+
+const app = express();
+app.get('/', (_: unknown, res: {send: (arg0: string) => unknown}) =>
+  res.send('Bot is alive!'),
+);
+app.listen(3000, () => console.log('Keep-alive server running'));
